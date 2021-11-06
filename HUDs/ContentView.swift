1036a9e647db0c122678ac10e33f15d16359f4f0
Atakan Cengiz KURT <atakan1903@hotmail.com>
2021-11-06 20:14:06 +0300

    Initial Commit

---
+++ HUDs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HUDs
+//
+//  Created by Atakan Cengiz KURT on 6.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HUDs/HUDsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HUDsApp.swift
+//  HUDs
+//
+//  Created by Atakan Cengiz KURT on 6.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct HUDsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
